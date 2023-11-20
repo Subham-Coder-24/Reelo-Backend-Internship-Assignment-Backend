06d7f8c4a33102d24d862b3a46d9bcdd73c1d67d
Subham Swarup Pradhan <subhamswaruppradhan2580@gmail.com>
2023-11-20 22:49:41 +0530

    add cors allow remote

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: "https://655b93c34cc967009304bff4--cool-smakager-a5b500.netlify.app",
       credentials: true,
     })
   );
